9e97bbe096147d3d75f9f1a2b1df0e812357f430
Raul Popadineti <raulpopadineti@gmail.com>
2013-06-22 13:11:12 +0300

    Removed exceptional gem for tracking errors after subscribing

--- app/controllers/spree/subscriptions_controller.rb
+++ app/controllers/spree/subscriptions_controller.rb
@@ -1,5 +1,3 @@
-require 'exceptional'
-
 class Spree::SubscriptionsController < Spree::BaseController
 
   def create
@@ -10,28 +8,12 @@ class Spree::SubscriptionsController < Spree::BaseController
     elsif params[:email] !~ /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
       @errors << t('invalid_email_address')
     else
-      begin
-          @mc_member = gibbon.member_info(Spree::Config.get(:mailchimp_list_id), params[:email])
-
-	rescue => ex
-
-	   Exceptional.handle ex
-
-      end
+      @mc_member = gibbon.member_info(Spree::Config.get(:mailchimp_list_id), params[:email])
 
       if @mc_member
         @errors << t('that_address_is_already_subscribed')
       else
-        begin
-            gibbon.subscribe({:id => Spree::Config.get(:mailchimp_list_id),:email_address =>  params[:email] })
-
-	 rescue => ex
-
-	   Exceptional.handle ex
-
-	 end
-
-
+        gibbon.subscribe({:id => Spree::Config.get(:mailchimp_list_id),:email_address =>  params[:email] })
       end
     end
 
@@ -42,8 +24,8 @@ class Spree::SubscriptionsController < Spree::BaseController
 
   private
 
-    def gibbon
-	@gibbon ||= Gibbon.new({ :api_key => Spree::Config.get(:mailchimp_api_key) })
-    end
+  def gibbon
+    @gibbon ||= Gibbon.new({ :api_key => Spree::Config.get(:mailchimp_api_key) })
+  end
 
 end
