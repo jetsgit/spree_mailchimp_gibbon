851a4b77535acf16202bff5de495c9248dd22a8b
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-02 20:17:59 +0800

    Delete app directory

--- app/overrides/add_newsletter_checkbox_to_create_account_page.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_user_form",
-                     :name         => "add_newsletter_checkbox_to_create_account_page",
-                     :insert_top   => "[data-hook='signup_below_password_fields']",
-                     :partial      => "spree/users/subscribe_to_newsletter_field" )
